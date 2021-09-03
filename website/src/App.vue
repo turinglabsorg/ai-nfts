56bae5f24707e48899ce818921bab8d48d389472
turinglabsorg <s.cataudo@turinglabs.org>
2021-09-03 12:45:14 +0200

    min fixes

--- website/src/App.vue
+++ website/src/App.vue
@@ -12,14 +12,14 @@ html {
   min-height: 100vh;
 }
 .home-text {
-  width:50%;
-  float:left;
-  text-align:center;
+  width: 50%;
+  float: left;
+  text-align: center;
 }
-.home-images{
-  width:50%;
-  float:left;
-  overflow:hidden;
+.home-images {
+  width: 50%;
+  float: left;
+  overflow: hidden;
   height: 100vh;
 }
 .button {
@@ -36,17 +36,33 @@ html {
   margin-top: 0px;
   font-size: 28px !important;
 }
-
-#nav {
-  padding: 30px;
+.title-drop{
+  font-size:60px!important;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+@media screen and (max-width: 768px) {
+  .home-images {
+    width: 100%;
+    position: fixed;
+    z-index: 5;
+  }
+  .home-text {
+    width: 100%;
+    position: absolute;
+    z-index: 10;
+    padding: 10px;
+    background-color: rgba(255, 249, 82, 0.8);
+    min-height: 100vh;
+    padding-bottom:80px;
+  }
+  #app {
+    font-size: 25px !important;
+  }
+  .button {
+    font-size: 20px !important;
+    margin-top: 40px;
+  }
+.title-drop{
+  font-size:30px!important;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
